5ac4c95ea714a3726e3ae48c158b4152c041e2f3
Joey Engel <github@engelcc.com>
2021-01-27 14:12:16 -0500

    Add VSCodium config

--- stow.sh
+++ stow.sh
@@ -5,12 +5,18 @@
 stowPkg="stow"
 packages="git hexchat terminator tmux"
 errorCount=0
+
 #-- Do Work
 if [[ $(dnf list installed|grep -c ${stowPkg}) -lt 1 ]]; then
   echo "${stowPkg} not installed.  To install, run \"sudo dnf install -y ${stowPkg}\""
   exit 1
 fi
-cd ~/dotfiles || ( echo "error" && exit 1 )
+
+echo "---- Starting: Stow Configurations ($(date)) ----"
+echo ""
+echo "Running: cd ~/dotfiles"
+cd ~/dotfiles || ( echo -e "Error: Dir not found. Exitting.\n" && exit 1 )
+
 #-- Majority configs
 for package in ${packages}; do
   if [[ $(dnf list installed|grep -c ${package}) -lt 1 ]]; then
@@ -20,16 +26,40 @@ for package in ${packages}; do
   fi
   echo "Running: stow ${package}"
   stow ${package}
+  echo ""
 done
+
+#-- VS Codium
+echo "Stowing Codum:"
+if [[ -f /usr/bin/codium ]]; then
+  if [[ -f ~/.config/VSCodium/User/settings.json ]]; then
+    echo "  Backing up current user settings:"
+    echo "   - Running: cp ~/.config/VSCodium/User/settings.json ~/.config/VSCodium/User/settings.json-$(date +%Y%m%d)-bak"
+    cp ~/.config/VSCodium/User/settings.json ~/.config/VSCodium/User/settings.json-$(date +%Y%m%d)-bak
+  fi
+  echo "  Running: stow VSCodium"
+  stow VSCodium
+else
+  echo "  Error: VSCodium not installed. Skipping."
+  ((errorCount++))
+fi
+echo ""
+
 #-- Gnome Themes
 echo "Running: stow themes"
 stow themes
+echo ""
+
 #-- fonts (requires sudo)
 echo "To install fonts, run:"
 echo -e "sudo cp -R fonts/* /usr/share/fonts\n"
+echo ""
+
 #-- Firefox
 echo "To add firefox custom preferences, move 'firefox/user.js' into the root directory of the firefox profile (~/.mozilla/firefox/..."
+
 #-- Exit
-echo "Complete!"
+echo -e  "\n---- Complete! ($(date))----"
 echo "Errors: ${errorCount}"
 exit ${errorCount}
+
